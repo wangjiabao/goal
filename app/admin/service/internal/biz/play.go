e61cfbb14daa3a0c9a43ef9098c545af74a8b8cc
wangjiabao <840028401@qq.com>
2022-11-07 13:43:12 +0800

    ok

--- app/admin/service/internal/biz/play.go
+++ app/admin/service/internal/biz/play.go
@@ -303,7 +303,7 @@ func (p *PlayUseCase) CreatePlayGame(ctx context.Context, req *v1.CreatePlayGame
 		return nil, errors.New(500, "TIME_ERROR", "时间输入错误")
 	}
 
-	if "game_team_goal" != req.SendBody.PlayType && // 验证type类型
+	if "game_team_goal_all" != req.SendBody.PlayType && // 验证type类型
 		"game_score" != req.SendBody.PlayType &&
 		"game_team_result" != req.SendBody.PlayType &&
 		"game_team_goal_up" != req.SendBody.PlayType &&
