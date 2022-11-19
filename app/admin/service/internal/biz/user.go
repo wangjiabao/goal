90a683a8e0aede120a7b184c7dede77ee346e492
wangjiabao <840028401@qq.com>
2022-11-19 21:57:06 +0800

    ok

--- app/admin/service/internal/biz/user.go
+++ app/admin/service/internal/biz/user.go
@@ -561,7 +561,7 @@ func (u *UserUseCase) CreateDownProxy(ctx context.Context, user *User, req *v1.C
 		return nil, errors.New(500, "USER_NO_FOUND", "用户地址有误")
 	}
 
-	systemConfig, err = u.systemConfigRepo.GetSystemConfigByNames(ctx, "result_play_rate")
+	systemConfig, err = u.systemConfigRepo.GetSystemConfigByNames(ctx, "down_proxy_rate")
 	if _, ok = systemConfig["result_play_rate"]; !ok {
 		return nil, errors.New(500, "USER_NO_FOUND", "配置有误")
 	}
