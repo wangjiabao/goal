23ae25aaab6283d473c7a8b877c5fe16ea79f24e
wangjiabao <840028401@qq.com>
2022-11-20 22:42:58 +0800

    ok

--- app/user/service/internal/biz/user.go
+++ app/user/service/internal/biz/user.go
@@ -109,26 +109,22 @@ func (uc *UserUseCase) EthAuthorize(ctx context.Context, u *User, req *v1.EthAut
 		privateKey        string
 		publicAddress     string
 		err               error
-		//decodeBytes       []byte
+		decodeBytes       []byte
 	)
 
 	user, err = uc.repo.GetUserByAddress(ctx, u.Address) // 查询用户
 	if nil == user || nil != err {
-		tmpRecommendCode := ""
-		//if "0x032d7e87ddceabc73447782676ab72aDC11D9870" != req.SendBody.Address {
-		//	code := req.SendBody.Code // 查询推荐码
-		//	decodeBytes, err = base64.StdEncoding.DecodeString(code)
-		//	code = string(decodeBytes)
-		//	if 0 == len(code) {
-		//		return nil, errors.New(500, "USER_ERROR", "无效的推荐码")
-		//	}
-		//
-		//	userInfo, err = uc.uiRepo.GetUserInfoByCode(ctx, code)
-		//	if err != nil {
-		//		return nil, errors.New(500, "USER_ERROR", "无效的推荐码")
-		//	}
-		//	tmpRecommendCode = userInfo.MyRecommendCode
-		//}
+		code := req.SendBody.Code // 查询推荐码
+		decodeBytes, err = base64.StdEncoding.DecodeString(code)
+		code = string(decodeBytes)
+		if 0 == len(code) {
+			return nil, errors.New(500, "USER_ERROR", "无效的推荐码")
+		}
+
+		userInfo, err = uc.uiRepo.GetUserInfoByCode(ctx, code)
+		if err != nil {
+			return nil, errors.New(500, "USER_ERROR", "无效的推荐码")
+		}
 
 		if privateKey, publicAddress = ethAccount(); 0 == len(privateKey) || 0 == len(publicAddress) {
 			return nil, errors.New(500, "USER_ERROR", "生成账户失败，请重试")
@@ -142,7 +138,7 @@ func (uc *UserUseCase) EthAuthorize(ctx context.Context, u *User, req *v1.EthAut
 				return err
 			}
 
-			userInfo, err = uc.uiRepo.CreateUserInfo(ctx, user, tmpRecommendCode) // 创建用户信息
+			userInfo, err = uc.uiRepo.CreateUserInfo(ctx, user, userInfo.MyRecommendCode) // 创建用户信息
 			if err != nil {
 				return err
 			}
@@ -152,6 +148,7 @@ func (uc *UserUseCase) EthAuthorize(ctx context.Context, u *User, req *v1.EthAut
 				return err
 			}
 
+			fmt.Println(user.ToAddress, u.ToAddress)
 			addressEthBalance, err = uc.abRepo.CreateAddressEthBalance(ctx, u.ToAddress)
 			if err != nil {
 				return err
